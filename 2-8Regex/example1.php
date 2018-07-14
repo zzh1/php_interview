aad325e5f06e240edbe4e14994c748f9f3930761
zhaozhonghao <775430005@qq.com>
2018-07-14 16:56:27 +0800

    mysql 高性能索引

--- 2-8Regex/example1.php
+++ 2-8Regex/example1.php
@@ -1,11 +1,10 @@
 <?php
-
 // 请写出以139开头的11位手机号码的正则表达式
 
 // 13988888888
 $str = '13981288888';
 $pattern = '/^139\d{8}$/';
 preg_match($pattern,$str,$match);
-var_dump($match);
-
+//var_dump($match);
+phpinfo();
 
